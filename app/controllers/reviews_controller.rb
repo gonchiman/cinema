6c6c74245979a24b43a9cad0f907540448112c8b
Ryo Yamamoto <legendaryyamasama@gmail.com>
2026-06-15 17:05:34 +0900

    Add user_id to review_params for user association in reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -65,6 +65,6 @@ class ReviewsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def review_params
-      params.expect(review: [ :content, :movie_id ])
+      params.expect(review: [ :content, :movie_id, :user_id ])
     end
 end
