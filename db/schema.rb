e359298b88ed7610f65ca180c458c5cb25620d86
Ryo Yamamoto <legendaryyamasama@gmail.com>
2026-06-15 16:19:33 +0900

    Update schema to include users table with email, name, and password_digest fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_08_085756) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_15_070904) do
   create_table "actors", force: :cascade do |t|
     t.date "birth_date"
     t.string "blood_type"
@@ -38,5 +38,24 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_08_085756) do
     t.index ["movie_id"], name: "index_reviews_on_movie_id"
   end
 
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "ip_address"
+    t.datetime "updated_at", null: false
+    t.string "user_agent"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email_address", null: false
+    t.string "name", null: false
+    t.string "password_digest", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
   add_foreign_key "reviews", "movies"
+  add_foreign_key "sessions", "users"
 end
