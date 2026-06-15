9d6c1182dfe3174a160850c965c552171895f4dd
Ryo Yamamoto <legendaryyamasama@gmail.com>
2026-06-15 17:37:05 +0900

    Add validation for points in reviews to ensure integer range

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,11 @@
 class Review < ApplicationRecord
   belongs_to :movie
   belongs_to :user
+
+  validates :points,
+            numericality: {
+              only_integer: true,
+              greater_than: 1,
+              less_than: 5
+            }
 end
