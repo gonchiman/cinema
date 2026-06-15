9b516620e6b4461d99cc0d9647fbe7ff92b125c1
Ryo Yamamoto <legendaryyamasama@gmail.com>
2026-06-15 17:38:06 +0900

    Add points parameter to review_params for scoring system

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -65,6 +65,6 @@ class ReviewsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def review_params
-      params.expect(review: [ :content, :movie_id, :user_id ])
+      params.expect(review: [ :content, :movie_id, :user_id, :points ])
     end
 end
